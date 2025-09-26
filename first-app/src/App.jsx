e1b60e4ca16ad5a9515724380ca3b3fa13db3b38
fabianoGDB <doctorfabi.f@gmail.com>
2025-09-25 23:48:34 -0300

    A006: adding use state to like a movie

--- first-app/src/App.jsx
+++ first-app/src/App.jsx
@@ -1,19 +1,28 @@
 import { useState } from "react";
 
-export const Card = ({ title }) => {
+export const Card = ({ title, hasLiked, handleHasLiked }) => {
+  const liked = hasLiked;
   return (
     <div className="card">
       <h2>{title}</h2>
+
+      <button onClick={() => handleHasLiked(true)}>Like</button>
     </div>
   );
 };
 
 const App = () => {
-  const [hasLiked, setHasLiked] = useState();
+  const [hasLiked, setHasLiked] = useState(false);
 
   return (
     <div className="card-container">
-      <Card title="Star Wars"></Card>
+      <Card
+        title="Star Wars"
+        rating={5}
+        isCool={true}
+        handleHasLiked={setHasLiked}
+        hasLiked={hasLiked}
+      ></Card>
       <Card title="Avatar"></Card>
       <Card title="Lion King"></Card>
     </div>
