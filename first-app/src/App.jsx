c10117644f5d3ba2f84b075497f7469e39975893
fabianoGDB <doctorfabi.f@gmail.com>
2025-09-25 23:51:06 -0300

    A007: moving the like logic inside the card component

--- first-app/src/App.jsx
+++ first-app/src/App.jsx
@@ -1,28 +1,20 @@
 import { useState } from "react";
 
-export const Card = ({ title, hasLiked, handleHasLiked }) => {
-  const liked = hasLiked;
+export const Card = ({ title }) => {
+  const [hasLiked, setHasLiked] = useState(false);
   return (
     <div className="card">
       <h2>{title}</h2>
 
-      <button onClick={() => handleHasLiked(true)}>Like</button>
+      <button onClick={() => setHasLiked(true)}>Like</button>
     </div>
   );
 };
 
 const App = () => {
-  const [hasLiked, setHasLiked] = useState(false);
-
   return (
     <div className="card-container">
-      <Card
-        title="Star Wars"
-        rating={5}
-        isCool={true}
-        handleHasLiked={setHasLiked}
-        hasLiked={hasLiked}
-      ></Card>
+      <Card title="Star Wars" rating={5} isCool={true}></Card>
       <Card title="Avatar"></Card>
       <Card title="Lion King"></Card>
     </div>
