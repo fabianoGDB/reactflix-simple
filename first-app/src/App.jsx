f210e83522d137b78785dc822390346e669f256b
fabianoGDB <doctorfabi.f@gmail.com>
2025-09-26 22:41:54 -0300

    A011: cleaning the project to start the reactflix project

--- first-app/src/App.jsx
+++ first-app/src/App.jsx
@@ -1,41 +1,7 @@
-import { useEffect, useState } from "react";
-
-export const Card = ({ title }) => {
-  const [count, setCount] = useState(0);
-  const [hasLiked, setHasLiked] = useState(false);
-
-  useEffect(() => {
-    console.log(`${title} has been liked: ${hasLiked}`);
-  }, [hasLiked]);
-
-  useEffect(() => {
-    console.log("CARD RENDERED");
-  }, []);
-
-  return (
-    <div
-      className="card"
-      onClick={() => setCount((prevState) => prevState + 1)}
-    >
-      <h2>
-        {title} - Likes ({count})
-      </h2>
-
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? "❤️" : "🤍"}
-      </button>
-    </div>
-  );
-};
+import React from "react";
 
 const App = () => {
-  return (
-    <div className="card-container">
-      <Card title="Star Wars" rating={5} isCool={true}></Card>
-      <Card title="Avatar"></Card>
-      <Card title="Lion King"></Card>
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
